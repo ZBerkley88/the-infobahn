47f50dac36ed86a3b4fd2047c6a522446b36b231
Zack <zachary.berkley@gmail.com>
2023-01-29 10:58:40 -0500

    Added index.js to /models, working on getting server to run

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
-const { connect, connection } = require("mongoose");
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 
 // Wrap Mongoose around local connection to MongoDB
 mongoose.connect(`mongodb://127.0.0.1:27017/infobahnDB`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
+});
 
 // Export connection 
 module.exports = mongoose.connection;
--- index.js
+++ index.js
@@ -1,29 +1,16 @@
 const express = require("express");
-const db = require(".config/connection");
+const db = require("./config/connection");
+const routes = require("./routes");
 
-const mongodb = require("mongodb").MongoClient;
-
-// import your models here
-
+const PORT = process.env.port || 3001;
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
-const connectionStringURI = `mongodb://127.0.0.1:27017/infobahnDB`;
-
-// Creates a connection to a MongoDB instance and returns the reference to the database
-mongodb.connect(
-  // Defines connection between app and MongoDB instance
-  connectionStringURI,
-  // Sets connection string parser and Server Discover and Monitoring engine to true and avoids warning
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    // Use client.db() constructor to add new db instance
-    db = client.db();
-    app.listen(PORT, () => {
-      console.log(`App listening at http://localhost:${PORT}`);
-    });
-  }
-);
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
